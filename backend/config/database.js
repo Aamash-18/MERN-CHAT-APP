a0ac6373c07c9b68b89b34756479262536a7ea18
Aamash-18 <aamashansari446@gmail.com>
2026-01-29 00:31:54 +0530

    database.js updataed

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,15 @@
-import mongoose from "mongoose"
-const connectDB=async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI).then(()=>{
-            console.log("DATABASE CONNECTED...");
-        })
-    }catch(err){
-        console.log(err);
-    }
-}
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // wait up to 30s for MongoDB
+    });
+
+    console.log("DATABASE CONNECTED...");
+  } catch (err) {
+    console.log("MongoDB connection error:", err);
+  }
+};
+
 export default connectDB;
