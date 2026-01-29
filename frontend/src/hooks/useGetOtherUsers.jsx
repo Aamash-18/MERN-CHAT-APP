56acb33be9189707957f53166097237858206662
Aamash-18 <aamashansari446@gmail.com>
2026-01-29 18:56:11 +0530

    changes in useGetOtherUserhook

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,25 +1,29 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setOtherUser } from "../redux/userSlice";
 
 const useGetOtherUsers = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    const fetchOhterUsers = async () => {
+    const fetchOtherUsers = async () => {
       try {
-        axios.defaults.withCredentials = true;
         const res = await axios.get(
           "https://mern-chat-app-0neo.onrender.com/api/v1/user/",
+          {
+            withCredentials: true,
+          },
         );
-        // console.log(res);
+
         dispatch(setOtherUser(res.data));
       } catch (err) {
         console.log(err);
       }
     };
-    fetchOhterUsers();
-  }, []);
+
+    fetchOtherUsers();
+  }, [dispatch]);
 };
 
 export default useGetOtherUsers;
